c1bd90744dbb50ab582e6983e561a4ab822bcb5e
andip71 <andreasp@gmx.de>
2017-08-28 14:57:18 +0200

    [SQUASH]drivers: Add boeffla wakelock blocker implementation
    Kanged from Haydn Agni Kernel.
    
    boeffla_wl_blocker: add generic wakelock blocker driver v1.0.0
    
    Based on ideas of FranciscoFranco's non-generic driver.
    
    Sysfs node:
    
    	/sys/class/misc/boeffla_wakelock_blocker/wakelock_blocker
    
    		- list of wakelocks to be blocked, separated by semicolons
    
    	/sys/class/misc/boeffla_wakelock_blocker/debug
    
    		- write: 0/1 to switch off and on debug logging into dmesg
    		- read:  get current driver internals
    
    	/sys/class/misc/boeffla_wakelock_blocker/version
    
    		- show driver version
    
    Signed-off-by: andip71 <andreasp@gmx.de>
    
    boeffla_wl_blocker: update to wakelock blocker driver v1.0.1
    
    - currently active wakelocks on the list are forcefully killed
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    boeffla_wl_blocker: update to wakelock blocker driver v1.1.0
    
    There are now two lists:
    - the previously existing list of user defined wakelocks to block
    - a new list called "wakelock_blocker_default" which comes prepopulated with the most common
      and safe wakelocks to block:
    
        qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK
    
    A combination of both wakelock lists will be blocked finally.
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    boeffla_wl_blocker: don't block wakelocks by default
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    power: boeffla blocker target IPA wakelocks
    
    power: boeffla blocker target qrtr_ws wakelock
    
    (cherry picked from commit b0d23e7c8032921b84cfb63b2dd714f3e0458318)
    
    Boeffla wakelock Blocker: disable NETLINK & [timerfd] wakelocks
    
    Boeffla wakelock Blocker: disable qup_uart wakelock
    
    Boeffla wakelock Blocker: disable rmnet_ipa wakelock
    
    Boeffla wakelock Blocker: disable wlan, tx_swr_ctrl & hal_bluetooth_lock wakelocks
    
    Boeffla wakelock Blocker: disable pmo_wow_wl wakelock
    
    Boeffla wakelock Blocker: option to disable default blocklist

---
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -0,0 +1,31 @@
+/*
+ * Author: andip71, 01.09.2017
+ *
+ * Version 1.1.0
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
+
+#ifdef CONFIG_BOEFFLA_WL_BLOCKER_APPLY_DEFAULT_BLOCKLIST
+#define LIST_WL_DEFAULT				"IPA_CLIENT_APPS_LAN_CONS;IPA_CLIENT_APPS_WAN_COAL_CONS;IPA_CLIENT_APPS_WAN_LOW_LAT_CONS;qrtr_ws;NETLINK;[timerfd];890000.qcom,qup_uart;rmnet_ipa%d;tx_swr_ctrl;wlan;hal_bluetooth_lock;pmo_wow_wl"
+#else
+#define LIST_WL_DEFAULT				""
+#endif
+
+#define LENGTH_LIST_WL				255
+#ifdef CONFIG_BOEFFLA_WL_BLOCKER_APPLY_DEFAULT_BLOCKLIST
+#define LENGTH_LIST_WL_DEFAULT		193
+#else
+#define LENGTH_LIST_WL_DEFAULT		0
+#endif
+#define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
