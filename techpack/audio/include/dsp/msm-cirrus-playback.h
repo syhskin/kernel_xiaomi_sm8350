6034370205d46ad3f7b06fc2fa77524023452688
syhskin <94190411+syhskin@users.noreply.github.com>
2024-03-09 03:40:04 +0800

    Update msm-cirrus-playback.h

--- techpack/audio/include/dsp/msm-cirrus-playback.h
+++ techpack/audio/include/dsp/msm-cirrus-playback.h
@@ -13,7 +13,7 @@
 #ifndef MSM_CIRRUS_PLAYBACK_H
 #define MSM_CIRRUS_PLAYBACK_H
 #define DEBUG
-#include <stdint.h>
+#include <dsp/stdint.h>
 #include <linux/slab.h>
 #include <sound/soc.h>
 #include <dsp/apr_audio-v2.h>
